7f31d1d96a5e347a6050eef4ef88a006f2bb779b
nikanoff <nikanoff@yandex.ru>
2024-05-03 19:52:55 +0300

    123

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -27,6 +27,7 @@ if "IS_DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
     ALLOWED_HOSTS = [os.environ["SITENAME"]]
+    CSRF_TRUSTED_ORIGINS = ["https://my.site.com"]
     
 else:
     SECRET_KEY = ["django-insecure-66=dw4_$(*jna4kpwo$%)3+$3g&_@xgcjp%cy)yaj+ni6((*fx"]
