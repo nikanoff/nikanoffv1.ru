6746548c201c4f157fd84b4d29f0ab1b6edfa9d5
nikanoff <nikanoff@yandex.ru>
2024-05-15 17:57:14 +0300

    five

--- nika/views.py
+++ nika/views.py
@@ -7,7 +7,7 @@ from .models import Article,Ip
 
 # Метод для получения айпи
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_REAL_IP')
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
